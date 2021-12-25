afe166e6846b6be13879f6d26708032bd78bafa9
Rodrigo <rodrigo.tavares@stone.com.br>
2021-12-25 15:57:10 -0300

    chore: remove lerna

--- packages/dynamic-string/src/index.ts
+++
@@ -1,8 +0,0 @@
-import * as R from 'ramda'
-
-const dynamicTemplate = R.curry(
-  (templateString: string, templateVariables: Record<string, any>) =>
-    templateString.replace(/\${(.*?)}/g, (_, g) => templateVariables[g])
-)
-
-export default dynamicTemplate
